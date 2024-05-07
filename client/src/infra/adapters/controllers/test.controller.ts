595d4529cb1f4d38af4d01b7facc20c84a68f617
Alex <wilmer.delgadoalama09@gmail.com>
2024-05-06 22:12:09 -0500

    fix: add dependencies to controller

--- client/src/infra/adapters/controllers/test.controller.ts
+++ client/src/infra/adapters/controllers/test.controller.ts
@@ -1,6 +1,7 @@
 import { controllerFactory } from "@/infra/dependencies/factories/controller.factory";
 
-const controller = controllerFactory().on((req, res) => {
+const controller = controllerFactory().on(({ req, res, dependencies }) => {
+  dependencies.logger.info("Test controller called");
   res.send(req.body);
 });
 
--- client/src/infra/utils/controller.ts
+++ client/src/infra/utils/controller.ts
@@ -1,15 +1,25 @@
 import { DependenciesDTO } from "../dependencies/dto/dependencies.dto";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+
+type ParamsHandle = {
+  req: Request;
+  res: Response;
+  dependencies: DependenciesDTO;
+  next: NextFunction;
+};
+
+type ControllerHandle = (params: ParamsHandle) => Promise<void> | void;
 
 export class Controller {
   dependencies: DependenciesDTO;
-  handle: (req: Request, res: Response) => Promise<void> | void;
+  handle: ControllerHandle;
 
   constructor(dependencies: DependenciesDTO) {
     this.dependencies = dependencies;
   }
 
-  on(handle: (req: Request, res: Response) => Promise<void> | void) {
+  on(handle: ControllerHandle) {
     this.handle = handle;
+    return this;
   }
 }
